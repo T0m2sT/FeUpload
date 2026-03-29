0b874845898414a2f99c17f6ea0e0fca1f7a39af
Tiago Su <tiagosu258@gmail.com>
2026-03-29 14:22:17 +0100

    Adding a list with objects which when clicked it opens the URL

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -1,22 +1,37 @@
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, TouchableOpacity, Linking } from "react-native";
 
 export default function ExercisesScreen() {
+  const exercises = [
+    {
+      id: "1",
+      title: "Workout Plan 1",
+      pdf: "https://pages.up.pt/~up353972/slides/?s=html5#1",
+    },
+  ];
+
+  const openPDF = (url: string) => {
+    Linking.openURL(url);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-      }}
-    >
-      <Text 
-        style={{
-          fontSize: 25,
-        }}
-      >
-        Exercises
-      </Text>
+    <View style={{ flex: 1, padding: 20 }}>
+      <FlatList
+        data={exercises}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => openPDF(item.pdf)}
+            style={{
+              padding: 15,
+              marginBottom: 10,
+              backgroundColor: "#eee",
+              borderRadius: 10,
+            }}
+          >
+            <Text style={{ fontSize: 18 }}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
