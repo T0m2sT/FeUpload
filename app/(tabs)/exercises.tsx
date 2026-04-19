1096919b1de0b2e180a13b8aca688db7c23fcacc
Pinho <rafaelpinhotsilva@gmail.com>
2026-04-19 17:19:00 +0100

    Fixed import missing

--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -2,6 +2,7 @@
 // This route is kept so expo-router doesn't error on the tab definition,
 // but it immediately redirects to home.
 import { Redirect } from 'expo-router';
+import { StyleSheet } from 'react-native';
 export default function ExercisesRedirect() {
   return <Redirect href="/" />;
 }
