3f63e69620b85bd14714d4aa25ebbbff3c62f19e
T0m2sT <pedrotomasteixeira@gmail.com>
2026-04-22 00:32:43 +0100

    test: add Jest testing framework and configuration

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'jest-expo',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: ['**/tests/**/*.test.ts', '**/tests/**/*.test.tsx'],
+  collectCoverageFrom: [
+    'services/**/*.ts',
+    'hooks/**/*.ts',
+    'contexts/**/*.tsx',
+    'constants/**/*.ts',
+    'components/**/*.tsx',
+    '!**/*.d.ts',
+  ],
+};
